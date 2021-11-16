7b32b03fc2075e79bd00fa5706e893c0eada01d8
wsxfvhu <krmiv77@gmail.com>
2021-11-16 18:38:36 +0300

    Добавьте файлы проекта.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "C:/Autodesk";
+            String[] direct = Directory.GetDirectories(path, "**", SearchOption.AllDirectories);
+            foreach (String d in direct)
+            {
+                Console.WriteLine("Подкаталог: {0}", d);
+                String[] files = Directory.GetFiles(d);
+                if (files.Length != 0)  
+                foreach (String f in files)
+                {
+                    Console.WriteLine("Файлы: {0}", f);
+                }
+                else Console.WriteLine("Файлов нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "C:/10chisel.txt";
+            Random random = new Random();
+            ushort n = 10;
+            int s = 0;
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    int d = random.Next(1, 100);
+                    sw.Write("{0} ",d);
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string read = sr.ReadToEnd();
+                string[] array = read.Split();
+                for (int i = 0; i < (array.Length - 1); i++)
+                {
+                    s += Convert.ToInt32(array[i]);
+                }
+                Console.Write(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
